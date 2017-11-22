0bbc1448aefa9566e23e04b18a6bcee892dba26f
Lauren <Laurenelee88@gmail.com>
2017-11-22 11:44:05 -0800

    working on submitting form to reserve trip

--- index.js
+++ index.js
@@ -46,35 +46,39 @@ $(document).ready(() => {
           event.stopPropagation();
         }); // stopping click from running too many times
 
+        $(this).one('click', 'p', function() {
+          let formInfo = `<form id="add-reservation" action="${individualURL}/reservations"><label for="name">Name:</label><input type="text" name="name"></input> <label for="age">Age:</label><input type="number" name="age"></input> <label for="email">Email:</label><input type="text" name="email"></input></form><div class="button"> <button type="submit">Make Reservation</button></div></form>`;
+
+          $(this).after(formInfo);
+          // figure out how to hide 'reserve today button'
+        }); // closing the .get
+        const successReservation = function successReservation() {
+          $("#message").html('<p> Reservation made! </p>');
+          console.log('successfully made reservation!');
+        };
+
         // begin .post to generate form and attach it to li
-        $(this).on('click', 'p', function() {
-          let formInfo = '<form id="add-reservation"><label for="name">Name:</label><input type="text" name="name"></input> <label for="age">Age:</label><input type="number" name="age"></input> <label for="email">Email:</label><input type="text" name="email"></input></form><div class="button"> <button type="submit" >Make Reservation</button></div>';
-
-          $(this).append(formInfo);
-
-          let reservationURL = `${individualURL}/reservations`;
+        $('#all-trips').on('submit','#add-reservation', function(event) {
+          event.preventDefault();
 
           let formData = $('#add-reservation').serialize();
 
-          const successReservation = function successReservation() {
-            $("#message").html('<p> Reservation made! </p>');
-            console.log('successfully made reservation!');
-          };
+          const reservationURL = $('#add-reservation').attr('action');
 
           $.post(reservationURL, formData, successReservation);
-          //     $("#message").html('<p> Reservation made! </p>');
-          //   }).fail(reservationFailureCallback);
-          // };
-          //
-          // let reservationFailureCallback = function(response) {
-          //   $("#all-trips").empty();
-          //   $(".errors").html("<h3>Sorry, that reservation attempt failed!</h3>");
+        });
+        //     $("#message").html('<p> Reservation made! </p>');
+        //   }).fail(reservationFailureCallback);
+        // };
+        //
+        // let reservationFailureCallback = function(response) {
+        //   $("#all-trips").empty();
+        //   $(".errors").html("<h3>Sorry, that reservation attempt failed!</h3>");
 
-          // would like to toggle back and forth.
-          // $(this).click((event) => {
-          //   event.stopPropagation();
-          // });
-        }); // closing the .get
+        // would like to toggle back and forth.
+        // $(this).click((event) => {
+        //   event.stopPropagation();
+        // });
 
       });
       // });
